f3d02f615d84d2e872895e3c01d5d106340151e1
Thomas William McClean <spam@tommcclean.me>
2020-07-06 23:22:45 +0100

    Add SquareButton Component

---
+++ src/components/SquareButton/src/SquareButton.vue
@@ -0,0 +1,73 @@
+<template functional>
+  <button
+    class="c-button"
+    :type="props.type"
+    :class="[
+      { 'u-primary': props.isPrimary },
+      { 'c-button-white' : props.isWhite }
+    ]"
+    @click="props.execute">
+    <span
+      v-text="props.text" />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: 'Submit'
+    },
+    isPrimary: {
+      type: Boolean,
+      default: false
+    },
+    isWhite: {
+      type: Boolean,
+      default: false
+    },
+    type: {
+      type: String,
+      default: 'submit'
+    },
+    execute: {
+      type: Function,
+      default: () => { }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.c-button {
+  display: inline-block;
+  padding: 15px 20px;
+  background-color: #EDEDED;
+  cursor: pointer;
+  border-radius: 5px;
+  border: 1px solid white;
+}
+
+.c-button:hover {
+  border: 1px solid #EDEDED;
+}
+
+.c-button.u-primary {
+  background-color: #003FBA;
+  color: white;
+}
+
+.c-button.u-primary:hover {
+  border: 1px solid #003FBA;
+}
+
+.c-button.c-button-white {
+  background-color: white;
+  border: 1px solid #EDEDED;
+}
+
+.c-button.c-button-white:hover {
+  border: 1px solid white;
+}
+</style>
---
+++ src/storybook/stories/SquareButton.stories.js
@@ -0,0 +1,29 @@
+import { withKnobs, text, select, boolean } from '@storybook/addon-knobs'
+
+import SquareButtonComponent from '../../components/SquareButton/src/SquareButton';
+
+export default {
+  title: 'Components',
+  decorators: [ withKnobs ]
+};
+
+export const SquareButton = () => ({
+  components: { SquareButtonComponent },
+  props: {
+    buttonText: {
+      default: text('Button Text', 'Square Button')
+    },
+    buttonType: {
+      default: select('Button Type', ['button', 'submit'])
+    },
+    isPrimary: {
+      default: boolean('Is Primary', false)
+    }
+  },
+  template: `
+    <square-button-component
+      :text="buttonText"
+      :type="buttonType"
+      :is-primary="isPrimary" />
+    `
+});
